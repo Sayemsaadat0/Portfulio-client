47e6e73a6c9174cad680422314ad6340eb047cf2
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-23 18:21:31 +0600

    hero section

--- src/Components/LayOut/Main/Main.jsx
+++ src/Components/LayOut/Main/Main.jsx
@@ -4,7 +4,7 @@ import Footar from "../../Pages/Shared/Footar/Footar";
 
 const Main = () => {
     return (
-        <div className="min-h-screen">
+        <div className="max-w-[1366px] mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footar></Footar>
--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,14 +1,37 @@
-import Container from "../Shared/Container/Container";
-import ContactUs from "./ContactUs/ContactUs";
-
-
+import Typewriter from 'typewriter-effect';
 const Home = () => {
+    const string = [
+        "MERN Stack",
+        "Front-End",
+        "Full Stack",
+        "React"
+    ]
     return (
         <section>
-            <div className="min-h-screen flex items-center">
-                sdsd
+            <div className="h-screen heroBg relative flex items-center px-2">
+                {/* left */}
+                <div className="absolute top-6 md:top-52 ">
+                    <h3 className="md:text-4xl font-bold ">Hey there!</h3>
+                    <h1 className="md:text-7xl md:mt-4  font-bold text-2xl">I am  <span className="text-[#9D3535]">Sayem</span></h1>
+                </div>
+
+                {/* right */}
+                <div className="absolute top-24  text-center  md:top-[400px] right-2 md:right-0 ">
+                    <h1 className="md:text-7xl font-bold text-xl ">
+                        <span className="text-[#9D3535] flex gap-2 md:gap-6">
+                           <span className='text-black'>A</span>
+                            <Typewriter
+                                options={{
+                                    strings: ["MERN Stack", "Front-End", "Full Stack", "React"],
+                                    autoStart: true,
+                                    loop: true,
+                                }}
+                            />
+                        </span>
+                        Developer
+                    </h1>
+                </div>
             </div>
-            {/* <ContactUs></ContactUs> */}
         </section>
     );
 };
