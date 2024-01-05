35c1583b7077664549d43b37013c217dab7d29c0
Sayemsaadat0 <sayemsaadat0@gmail.com>
2024-01-05 22:43:35 +0600

    new commit

--- src/Components/Pages/Home/Banner.jsx
+++ src/Components/Pages/Home/Banner.jsx
@@ -2,11 +2,13 @@ import { motion } from "framer-motion"
 
 const Banner = () => {
     return (
-        <div  className="rounded-lg overflow-hidden">
-            <div className="flex justify-between items-center mt-2 image">
+        <div className="rounded-lg overflow-hidden">
+            {/* image */}
+            <div className="flex justify-between items-center mt-2 ">
+                <img className='object-cover hover:scale-105 shadow-lg rounded-lg h-40 duration-700 ' src="https://i.ibb.co/n84r1gC/20231213-104046f-1-2.jpg" alt="" />
                 {/* <img className='object-cover hover:scale-105 shadow-lg rounded-lg h-44 duration-700 ' src="/sayem.png" alt="" /> */}
             </div>
-    </div>
+        </div>
     );
 };
 
