bd010b0cb2cbf7afd68c808aec7fc4def514667d
Sayemsaadat0 <sayemsaadat0@gmail.com>
2024-01-08 22:45:50 +0600

    whatsapp add

--- src/Components/LayOut/Main/Main.jsx
+++ src/Components/LayOut/Main/Main.jsx
@@ -1,10 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import Navbar from "../../Pages/Shared/Navbar/Navbar";
 import Footar from "../../Pages/Shared/Footar/Footar";
 
 const Main = () => {
     return (
-        <div className="max-w-[1366px] mx-auto">
+        <div className="max-w-[1366px] mx-auto relative">
+            <div className="fixed right-10 top-[calc(100vh-100px)] z-10">
+                <a href="https://wa.me/01521574741">
+                    <img
+                        src="https://img.icons8.com/color/48/000000/whatsapp.png"
+                        alt="whatsapp"
+                    />
+                </a>
+                {/* <a aria-label="Chat on WhatsApp" href="https://wa.me/1XXXXXXXXXX"> <img alt="Chat on WhatsApp" src="WhatsAppButtonGreenLarge.png" />
+                    <a /> */}
+            </div>
+            {/* <Link to={'https://wa.me/01521574741'}>
+                <img src=""/>
+            </Link> */}
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footar></Footar>
