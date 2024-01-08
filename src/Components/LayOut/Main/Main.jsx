5f658255a3c0c3c1687df3bb3a156d571b42c770
Sayemsaadat0 <sayemsaadat0@gmail.com>
2024-01-08 23:00:17 +0600

    whatsapp

--- src/Components/LayOut/Main/Main.jsx
+++ src/Components/LayOut/Main/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
     return (
         <div className=" mx-auto px-1 md:px-4 relative">
             <div className="fixed right-6 xl:right-10 bottom-4 xl:top-[calc(100vh-100px)] z-10">
-                <a href="https://wa.me/01521574741">
+                <a href="https://wa.me/+08801521574741">
                     <img
                         src="https://img.icons8.com/color/48/000000/whatsapp.png"
                         alt="whatsapp"
