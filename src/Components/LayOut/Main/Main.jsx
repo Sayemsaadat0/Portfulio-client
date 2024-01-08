c945fd77b8d1581b50a2fe287efc4837d3e8c260
Sayemsaadat0 <sayemsaadat0@gmail.com>
2024-01-08 22:49:33 +0600

    whatsapp

--- src/Components/LayOut/Main/Main.jsx
+++ src/Components/LayOut/Main/Main.jsx
@@ -4,7 +4,15 @@ import { Outlet } from "react-router-dom";
 
 const Main = () => {
     return (
-        <div className=" mx-auto px-1 md:px-4">
+        <div className=" mx-auto px-1 md:px-4 relative">
+            <div className="fixed right-6 xl:right-10 bottom-4 xl:top-[calc(100vh-100px)] z-10">
+                <a href="https://wa.me/01521574741">
+                    <img
+                        src="https://img.icons8.com/color/48/000000/whatsapp.png"
+                        alt="whatsapp"
+                    />
+                </a>
+            </div>
             <Outlet></Outlet>
         </div>
     );
